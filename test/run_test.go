a81e2bcc3db1f1a7b65c179f7a7f5cf6921d4a6f
Joshua Nario <joshua.nario@hotmail.com>
2021-08-12 23:50:39 -0400

    1.0.0

--- test/run_test.go
+++ test/run_test.go
@@ -2,11 +2,13 @@ package test_test
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
 
 	"ecksbee.com/telefacts-taxonomy-package/pkg/install"
+	"ecksbee.com/telefacts-taxonomy-package/pkg/taxonomies"
 )
 
 func Test_Run_USGAAP2020(t *testing.T) {
@@ -33,6 +35,36 @@ func Test_Run_ESEF2017(t *testing.T) {
 	tp := filepath.Join(wd, "esef_taxonomy_2017.zip")
 	volume := filepath.Join(wd, "data")
 	id, err := install.Run(tp, volume)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if id == "" {
+		t.Fatal(fmt.Errorf("empty id generated"))
+	}
+}
+
+func Test_Run_CMF_CL_CI2020(t *testing.T) {
+	tm := taxonomies.Meta{
+		Name: "CMF CL-CI 2020",
+		Zip:  "articles-28084_recurso_1.zip",
+		Entries: []string{
+			"http://www.cmfchile.cl/cl/fr/ci/2020-01-02/cl-ci_shell_2020-01-02.xsd",
+		},
+		Remap: map[string]string{
+			"": "http://www.cmfchile.cl/cl/fr/ci/2020-01-02/",
+		},
+	}
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	tp := filepath.Join(wd, tm.Zip)
+	bytes, err := ioutil.ReadFile(tp)
+	if err != nil {
+		t.Fatal(err)
+	}
+	taxonomies.VolumePath = filepath.Join(wd, "data")
+	id, err := taxonomies.NewTaxonomy(tm, bytes)
 	if err != nil {
 		t.Fatal(err)
 	}
