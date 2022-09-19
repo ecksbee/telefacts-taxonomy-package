ec7d702a2764c183ed57ff9106f9e749c3d1f7bc
Joshua Nario <joshuanario@users.noreply.github.com>
2022-09-18 20:40:51 -0400

    telefacts-ns-mgr

--- cmd/telefacts-taxonomy-package/main.go
+++ cmd/telefacts-taxonomy-package/main.go
@@ -73,7 +73,7 @@ func setupServer() *http.Server {
 	cache.InitRepo(serializables.GlobalTaxonomySetPath)
 	r := web.NewRouter()
 
-	fmt.Println("telefacts-taxonomy-package-manager<-0.0.0.0:8080")
+	fmt.Println("telefacts-ns-mgr<-0.0.0.0:8080")
 	return &http.Server{
 		Addr:         "0.0.0.0:8080",
 		WriteTimeout: time.Second * 15,
